1d47b4b44302de321936031204f823833e72ef2f
TarasKing <ivanovtaras@live.ru>
2024-01-12 22:32:16 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+let fighting;
+let monsterHealth;
+let inventory = ["stick"];
+
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+
+let text = document.querySelector("#text");
+let xpText = document.querySelector("#xpText");
+let healthText = document.querySelector("#healthText");
+let goldText = document.querySelector("#goldText");
+let monsterStats = document.querySelector("#monsterStats");
+let monsterName = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
